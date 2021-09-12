b0b961d80fa97d09bce9b4e3613998459b50e400
StandingPadAnimations <75058058+StandingPadAnimations@users.noreply.github.com>
2021-09-11 22:39:26 -0500

    Add files via upload

---
+++ src/Lexer.cpp
@@ -0,0 +1,116 @@
+#include "lexer.hpp"
+using namespace Lex;
+
+
+std::vector<std::pair<Token, std::string_view>> Lexer::Tokenize(std::string& Code){
+    std::stringstream ss(Code);
+    std::string LineofCode;
+    std::vector<std::pair<Token, std::string_view>> Tokens;
+    unsigned int LineNumber = 0;
+
+    while(std::getline(ss, LineofCode)){
+        ++LineNumber;
+        // Check for beginning of string
+        ID TempID = ID::None;
+        std::string TempString = "";
+
+        for(size_t i = 0; i < LineofCode.size(); ++i){
+            // Checks for beginning and end of a string
+            if((LineofCode[i] == '\"')){
+                // beggining of a string
+                if(TempID == ID::None){
+                    TempID = ID::CharDouble;
+                    TempString = "";
+                }
+
+                // end of a string
+                else if(TempID == ID::CharDouble){
+                    std::cout << TempString << std::endl;
+                    TempID = ID::StringEnd;
+                    Tokens.emplace_back(Token::String, TempString);
+                    TempString = "";
+                }
+
+                else{
+                    throw error::RendorException("Invalid Syntax: EOL while scanning string on line " + std::to_string(LineNumber));
+                }
+            }
+
+            if((LineofCode[i] == '\'')){
+                // beggining of a string
+                if(TempID == ID::None){
+                    TempID = ID::CharArpro;
+                    TempString = "";
+                }
+
+                // end of a string
+                else if(TempID == ID::CharArpro){
+                    std::cout << TempString << std::endl;
+                    TempID = ID::StringEnd;
+                    Tokens.emplace_back(Token::String, TempString);
+                    TempString = "";
+                }
+
+                else{
+                    throw error::RendorException("Invalid Syntax: EOL while scanning string on line " + std::to_string(LineNumber));
+                }
+            }
+
+            else if((LineofCode[i] == ' ') && ((TempID != ID::CharDouble) || (TempID != ID::CharArpro))){
+                
+            }
+
+            // Comments
+            else if(((LineofCode[i] == '/') && (TempID == ID::None))){
+                if(TempID != ID::Comment){
+                    TempID = ID::Comment;
+                    TempString = "";
+                }
+                
+                else if(TempID == ID::Comment){
+                    break;
+                }
+            }
+
+            // Checks for keywords
+            else if((std::find(std::begin(Keywords), std::end(Keywords), TempString) != std::end(Keywords)) && ((TempID != ID::CharArpro) || (TempID != ID::CharDouble))){
+                std::cout << TempString << std::endl;
+                Tokens.emplace_back(Token::Keyword, TempString);
+                TempID = ID::KeywordArgs;
+            }
+
+            //Checks for parens
+            else if((LineofCode[i] == '(') && (TempID == ID::KeywordArgs)){
+                Tokens.emplace_back(Token::Paren, TempString);
+                TempID = ID::ParenArgs;
+                TempString = "";
+            }
+
+            else if((LineofCode[i] == ')') && (TempID == ID::ParenArgs)){
+                if(TempID == ID::ParenArgs){
+                    Tokens.emplace_back(Token::Paren, TempString);
+                    TempID = ID::None;
+                    TempString = "";
+                }
+
+                // in case a random ) is found
+                else{
+                    throw error::RendorException("Invalid Syntax: Random ) found on line " + LineNumber);
+                }
+            }
+
+            // Whenever { is used outside of a string, TempID will always be ID::None
+            else if((LineofCode[i] == '{') && (TempID == ID::None)){
+                Tokens.emplace_back(Token::Bracket, TempString);
+                TempID = ID::None;
+                TempString = "";
+            }
+
+            // Push chars in a temporary string
+            else{
+                TempString.push_back(LineofCode[i]);
+            }
+        }
+    }
+    return Tokens;
+}
