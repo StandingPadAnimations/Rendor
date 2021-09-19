15e528c54936720caab3cb6fcca58f4a43131466
StandingPadAnimations <75058058+StandingPadAnimations@users.noreply.github.com>
2021-09-18 19:52:47 -0500

    Add files via upload

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -2,10 +2,10 @@
 using namespace Lex;
 
 
-std::vector<std::pair<Token, std::string_view>> Lexer::Tokenize(std::string& Code){
+std::vector<std::pair<Token, std::string>> Lexer::Tokenize(std::string& Code){
     std::stringstream ss(Code);
     std::string LineofCode;
-    std::vector<std::pair<Token, std::string_view>> Tokens;
+    std::vector<std::pair<Token, std::string>> Tokens;
     unsigned int LineNumber = 0;
 
     while(std::getline(ss, LineofCode)){
@@ -19,21 +19,21 @@ std::vector<std::pair<Token, std::string_view>> Lexer::Tokenize(std::string& Cod
             if((LineofCode[i] == '\"') || (LineofCode[i] == '\'')){
                 // beggining of a string
                 if((TempID.back() == ID::None) || (TempID.back() == ID::ParenArgs) || (TempID.back() == ID::KeywordArgs)){
-                    std::cout << "StringStart " << TempString << std::endl;
+                    // std::cout << "StringStart " << TempString << std::endl;
                     TempID.emplace_back(ID::Char);
                     TempString = "";
                 }
 
                 // end of a string
                 else if(TempID.back() == ID::Char){
-                    std::cout << TempString << std::endl;
+                    // std::cout << TempString << std::endl;
                     Tokens.emplace_back(Token::String, TempString);
                     TempID.pop_back();
                     TempString = "";
                 }
 
                 else{
-                    std::cout << "StringEOL Error " << TempString  << " " << static_cast<std::underlying_type<ID>::type>(TempID.back()) << std::endl;
+                    // std::cout << "StringEOL Error " << TempString  << " " << static_cast<std::underlying_type<ID>::type>(TempID.back()) << std::endl;
                     throw error::RendorException("Invalid Syntax: EOL while scanning string on line " + std::to_string(LineNumber));
                 }
             }
@@ -45,44 +45,29 @@ std::vector<std::pair<Token, std::string_view>> Lexer::Tokenize(std::string& Cod
             // Comments
             else if((LineofCode[i] == '/') && (TempID.back() != ID::Char)){
                 if(TempID.back() != ID::Comment){
-                    std::cout << "Encounted Comment" << std::endl;
+                    // std::cout << "Encounted Comment" << std::endl;
                     TempID.emplace_back(ID::Comment);
                     TempString = "";
                 }
                 
                 else if(TempID.back() == ID::Comment){
-                    std::cout << "Comment " << TempString << std::endl; 
+                    // std::cout << "Comment " << TempString << std::endl; 
                     break;
                 }
             }
 
             // Checks for keywords
             else if((std::find(std::begin(Keywords), std::end(Keywords), TempString) != std::end(Keywords)) && (TempID.front() == ID::None)){
-                std::cout << "Keyword " << TempString << std::endl;
+                // std::cout << "Keyword " << TempString << std::endl;
                 Tokens.emplace_back(Token::Keyword, TempString);
                 TempID.emplace_back(ID::KeywordArgs);
                 TempString = "";
             }
 
             //Checks for parens
-            else if((LineofCode[i] == '(') && (TempID.back() == ID::KeywordArgs)){
-                Tokens.emplace_back(Token::Paren, TempString);
-                TempID.pop_back();
-                TempID.emplace_back(ID::ParenArgs);
+            else if(((LineofCode[i] == '(') || (LineofCode[i] == ')')) && (TempID.back() == ID::KeywordArgs)){
+                Tokens.emplace_back(Token::Paren, "PAREN");
                 TempString = "";
-            }
-
-            else if((LineofCode[i] == ')') && (TempID.back() == ID::ParenArgs)){
-                if(TempID.back() == ID::ParenArgs){
-                    Tokens.emplace_back(Token::Paren, TempString);
-                    TempID.pop_back();
-                    TempString = "";
-                }
-
-                // in case a random ) is found
-                else{
-                    throw error::RendorException("Invalid Syntax: Random ) found on line " + LineNumber);
-                }
             }
 
             // Whenever { is used outside of a string, TempID will always be ID::None
@@ -92,15 +77,26 @@ std::vector<std::pair<Token, std::string_view>> Lexer::Tokenize(std::string& Cod
                 TempString = "";
             }
 
-            else if((LineofCode[i] == '|') && (TempID.back() == ID::ParenArgs)){
-                Tokens.emplace_back(Token::Operator, TempString);
+            else if((LineofCode[i] == '#') && (TempID.back() != ID::Char)){
+                throw error::RendorException("Preprocessor definitions are currently not supported in Rendor 1.0");
+            }
+
+            else if((LineofCode[i] == '|') && (TempID.back() == ID::KeywordArgs)){
+                std::cout << "Reached Operator " << TempString << std::endl;
+                Tokens.emplace_back(Token::Operator, "OR");
+                TempString = "";
+            }
+
+            else if((LineofCode[i] == '&') && (TempID.back() == ID::KeywordArgs)){
+                std::cout << "Reached Operator " << TempString << std::endl;
+                Tokens.emplace_back(Token::Operator, "AND");
                 TempString = "";
             }
 
             // Push chars in a temporary string
             else{
                 TempString.push_back(LineofCode[i]);
-                std::cout << static_cast<std::underlying_type<ID>::type>(TempID.back()) << std::endl;
+                // std::cout << static_cast<std::underlying_type<ID>::type>(TempID.back()) << std::endl;
             }
         }
     }
