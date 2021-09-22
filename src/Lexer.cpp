1650e46790ac0cc55be888aa3ecaf8ccc3a74f4f
StandingPadAnimations <75058058+StandingPadAnimations@users.noreply.github.com>
2021-09-21 19:11:45 -0500

    Add files via upload
    Fixed bugs with (), {}, added ComparisonObject, and the == operator

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -83,31 +83,50 @@ std::vector<std::pair<Token, std::string>> Lexer::Tokenize(std::string& Code){
                 }
             }
 
-            // Checks for keywords
-            else if((std::find(std::begin(Keywords), std::end(Keywords), TempString) != std::end(Keywords)) && (TempID == ID::None)){
-                // std::cout << "Keyword " << TempString << std::endl;
-                Tokens.emplace_back(Token::Keyword, TempString);
-                TempIDList.emplace_back(ID::KeywordArgs);
-                TempString = "";
-            }
-
             // Checks for operators
             else if((std::find(std::begin(Operators), std::end(Operators), TempString) != std::end(Operators)) && (TempID == ID::KeywordArgs)){
                 Tokens.emplace_back(Token::Operator, TempString);
                 TempString = "";
+
+                // to account for chars not being added when a operator is found
+                TempString.push_back(LineofCode[i]);
             }
 
             //Checks for parens
-            else if(((LineofCode[i] == '(') || (LineofCode[i] == ')')) && (TempID == ID::KeywordArgs)){
+            else if(LineofCode[i] == '('){
+                // Sometimes the ( is part of a keyword
+                if((std::find(std::begin(Keywords), std::end(Keywords), TempString) != std::end(Keywords)) && (TempID == ID::None)){
+                    Tokens.emplace_back(Token::Keyword, TempString);
+                    TempIDList.emplace_back(ID::KeywordArgs);
+                    TempString = "";
+                }
                 Tokens.emplace_back(Token::Paren, "PAREN");
-                TempString = "";
             }
 
-            // Whenever { is used outside of a string, TempID will always be ID::KeywordArgs
-            else if((LineofCode[i] == '{') && (TempID == ID::KeywordArgs)){
-                Tokens.emplace_back(Token::Bracket, "BRACK");
-                TempIDList.pop_back();
-                TempString = "";
+            else if(LineofCode[i] == ')'){
+                Tokens.emplace_back(Token::Paren, "PAREN");
+            }
+
+            else if((LineofCode[i] == '{') || (LineofCode[i] == '}')){
+                switch(LineofCode[i]){
+                    case '{':
+                        switch(TempID){
+                            // Whenever { is used outside of a string, TempID will always be ID::KeywordArgs
+                            case ID::KeywordArgs:
+                                Tokens.emplace_back(Token::Bracket, "BRACK");
+                                TempIDList.pop_back();
+                                TempString = "";
+                                break;
+                            
+                            default:
+                                throw error::RendorException("Random { found on line " + std::to_string(LineNumber));
+                        }
+                        break;
+
+                    case '}':
+                        Tokens.emplace_back(Token::Bracket, "BRACK");
+                        break;
+                }
             }
 
             // Preprocessor definitions are planned for a future update 
@@ -127,11 +146,59 @@ std::vector<std::pair<Token, std::string>> Lexer::Tokenize(std::string& Code){
                             SpecificTempID = SpecificID::EqualOperator;
                             break;
                         }
+                        Tokens.emplace_back(Token::ComparisonObject, TempString);
                         Tokens.emplace_back(Token::Operator, "EQUAL");
+                        TempString = "";
                         break;
 
                     default:
                         throw error::RendorException("Random = found on line " + std::to_string(LineNumber));
+                }
+            }
+
+            else if(LineofCode[i] == '^' ||
+            LineofCode[i] == '*' || LineofCode[i] == '/' ||
+            LineofCode[i] == '+' || LineofCode[i] == '-'){
+                switch(LineofCode[i]){
+                    case '^':
+                        Tokens.emplace_back(Token::Arithmic, "^");
+                        break;
+
+                    case '*':
+                        Tokens.emplace_back(Token::Arithmic, "*");
+                        break;
+
+                    case '/':
+                        Tokens.emplace_back(Token::Arithmic, "/");
+                        break;
+
+                    case '+':
+                        switch(SpecificTempID){
+                            case SpecificID::IncrementDecrementCheck:
+                                Tokens.pop_back();
+                                Tokens.emplace_back(Token::Increment, "++");
+                                break;
+
+                            default:
+                                Tokens.emplace_back(Token::Arithmic, "+");
+                                SpecificTempID = SpecificID::IncrementDecrementCheck;
+                                break;
+                        }
+                        break;
+
+                    case '-':
+                        switch(SpecificTempID){
+                            case SpecificID::IncrementDecrementCheck:
+                                Tokens.pop_back();
+                                Tokens.emplace_back(Token::Decrement, "--");
+                                break;
+
+                            default:
+                                Tokens.emplace_back(Token::Arithmic, "-");
+                                SpecificTempID = SpecificID::IncrementDecrementCheck;
+                                break;
+                        }
+                        break;
                 }
             }
 
