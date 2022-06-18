4b3061ce08aedc7a5c817423bcea3257ae408e29
StandingPad <sheikhmahid1@gmail.com>
2022-06-18 13:40:23 -0500

    Fixed parser bug

--- src/RendorCompiler/Parser/ParsePrimary.cpp
+++ src/RendorCompiler/Parser/ParsePrimary.cpp
@@ -12,17 +12,19 @@ void RendorParser::PrimaryParse()
             }
             case LexTok::KEYWORD:
             {
-                GetNextTok();
                 if (CurrentValue == "let")
                 {
+                    GetNextTok();
                     ParseIdentifer(std::string{CurrentValue});
                 }
                 else if (CurrentValue == "edef")
                 {
+                    GetNextTok();
                     ParseEdef(std::string{CurrentValue});
                 }
                 else if (CurrentValue == "import")
                 {
+                    GetNextTok();
                     ParseImport(std::string{CurrentValue});
                 }
                 break;
